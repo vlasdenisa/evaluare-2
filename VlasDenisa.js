1c12703f281c0fe4d8f2470ee3792175e532593c
vlasdenisa <92874823+vlasdenisa@users.noreply.github.com>
2021-11-20 19:43:52 +0200

    Add files via upload

---
+++ VlasDenisa.js
@@ -0,0 +1,50 @@
+var xValues = ["Mascara", "Ruj", "Pudra praf", "Corector", "Fard pleoape"];
+var yValues = [39.15, 74.50, 17, 30, 23];
+var barColors = [
+  "#b91d47",
+  "#00aba9",
+  "#2b5797",
+  "#e8c3b9",
+  "#1e7145"
+];
+
+new Chart("myChart", {
+  type: "doughnut",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      
+    }
+  }
+});
+
+function monotorizare()
+{
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000);
+}
+
+}
